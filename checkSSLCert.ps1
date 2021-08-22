e34afd23d453e38562b4f3ec91aa0573876c37f7
Zain Raza <89275317+zainraza09@users.noreply.github.com>
2021-08-22 00:13:17 -0400

    Create  checkSSLCert.ps1 file
    This file check the Domain SSL cert Expiration date

---
+++ checkSSLCert.ps1
@@ -0,0 +1,10 @@
+$url = "https://www.microsoft.com/"
+[Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+$req = [Net.HttpWebRequest]::Create($url)
+$req.GetResponse() | Out-Null
+$result = [PSCustomObject]@{
+   URL = $url
+   'Cert Start Date' = $req.ServicePoint.Certificate.GetEffectiveDateString()
+   'Cert End Date' = $req.ServicePoint.Certificate.GetExpirationDateString()
+}
+$result
